3f3d3e9805d74eac9b8dc5bf0c6979a7e28d7485
pias97 <ranabir.devnath@gmail.com>
2023-12-16 16:47:41 +0600

    loc/prod script update

--- shells/run_loc.sh
+++ shells/run_loc.sh
@@ -4,3 +4,7 @@ sudo systemctl start mysql.service
 
 cp env_loc .env
 python3 main.py
+
+sudo docker compose down
+
+sudo docker compose up -d
--- shells/run_prod.sh
+++ shells/run_prod.sh
@@ -4,3 +4,7 @@ sudo systemctl start mysql.service
 
 cp env_pod .env
 python3 main.py
+
+sudo docker compose down
+
+sudo docker compose up -d
